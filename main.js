cb61fecf7e4fc13c33df051b2d21289cc2264d7f
Aditya Veer Parmar <nakulparmar15@gmail.com>
2023-12-14 12:12:08 +0530

    update stun servers

--- main.js
+++ main.js
@@ -13,7 +13,28 @@ let servers = {
     },
   ],
 };
-let peerConnection = new RTCPeerConnection(servers);
+
+let publicServers = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+    {
+      urls: "stun:stun1.l.google.com:19302",
+    },
+    {
+      urls: "stun:stun2.l.google.com:19302",
+    },
+    {
+      urls: "stun:stun3.l.google.com:19302",
+    },
+    {
+      urls: "stun:stun4.l.google.com:19302",
+    },
+  ],
+};
+
+let peerConnection = new RTCPeerConnection(publicServers);
 
 let init = async () => {
   console.log("Init triggered");
